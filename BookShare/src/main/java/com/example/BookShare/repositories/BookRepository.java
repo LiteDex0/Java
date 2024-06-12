ce7dcb8b4b5791a134f7be8b7d68756ef938fde2
LiteDex0 <115341556+LiteDex0@users.noreply.github.com>
2024-06-12 21:16:39 +0300

    Add files via upload

---
+++ BookShare/src/main/java/com/example/BookShare/repositories/BookRepository.java
@@ -0,0 +1,11 @@
+package com.example.BookShare.repositories;
+
+import com.example.BookShare.models.Book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByTitle(String title);
+}
