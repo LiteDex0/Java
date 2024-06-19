a8ab0f0420712e49634de664c17f6fdf0a1a1013
LiteDex0 <115341556+LiteDex0@users.noreply.github.com>
2024-06-19 12:19:38 +0300

    Delete BookShare directory

--- BookShare/src/main/java/com/example/BookShare/repositories/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BookShare.repositories;
-
-import com.example.BookShare.models.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
